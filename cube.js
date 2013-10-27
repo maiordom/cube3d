2d172a1892f35bf9f175ae2d131165fac2654789
maiordom <maiordom@yandex.ru>
2013-10-27 21:21:57 +0400

    update

--- cube.js
+++ cube.js
@@ -1,33 +1,31 @@
-(function( root ) {
+(function( root, undefined ) {
 
-var xAngle = 0, yAngle = 0;
-    cube = document.querySelector( '.cube' ),
-    transformPropName = getTransformPropNameByPrefix();
+    var xAngle = 0, yAngle = 0,
+        cube = document.querySelector( '.cube' ),
+        transformPropName = getTransformPropNameByPrefix();
 
-function getTransformPropNameByPrefix() {
-    var props = 'transform WebkitTransform MozTransform OTransform msTransform'.split( ' ' ),
-    prop,
-    el = document.createElement( 'div' );
+    function getTransformPropNameByPrefix() {
+        var props = 'transform WebkitTransform MozTransform OTransform msTransform'.split( ' ' ),
+            el = document.createElement( 'div' );
 
-    for ( var i = 0, ilen = props.length; i < ilen; i++ ) {
-        if ( typeof el.style[ props[ i ] ] !== "undefined" ) {
-            return props[ i ];
-            break;
+        for ( var i = 0, ilen = props.length; i < ilen; i++ ) {
+            if ( typeof el.style[ props[ i ] ] !== "undefined" ) {
+                return props[ i ];
+            }
         }
     }
-}
 
-function keydownHandler( e ) {
-    switch( e.keyCode ) {
-        case 37: yAngle -= 90; break; // left
-        case 38: xAngle += 90; break; // up
-        case 39: yAngle += 90; break; // right
-        case 40: xAngle -= 90; break; // down
-    };
+    function keydownHandler( e ) {
+        switch( e.keyCode ) {
+            case 37: yAngle -= 90; break; // left
+            case 38: xAngle += 90; break; // up
+            case 39: yAngle += 90; break; // right
+            case 40: xAngle -= 90; break; // down
+        }
 
-    cube.style[ transformPropName ] = "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)";
-}
+        cube.style[ transformPropName ] = "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)";
+    }
 
-document.body.onkeydown = keydownHandler;  
+    document.body.onkeydown = keydownHandler;
 
-})( window );  
+})( window, undefined );
